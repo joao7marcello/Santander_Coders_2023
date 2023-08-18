ba9286e4b2a3d0d2a539473e6189d6e75a37c0be
JM <jaomarcello7@gmail.com>
2023-08-18 14:32:52 -0300

    Projeto Individual Finalizado

--- santanders-coders-logica-de-prog/projeto_envio_emails/clientes.js
+++ santanders-coders-logica-de-prog/projeto_envio_emails/clientes.js
@@ -1,23 +1,23 @@
 const clientes = [
   {
-    addressee: "cliente1@projeto.com",
+    addressee: "cliente01@projeto.com",
     receberMarketing: true,
-    ultimaCompra: "12/07/2023",
+    ultimaCompra: "2023-07-29",
   },
   {
-    addressee: "cliente2@projeto.com",
+    addressee: "cliente02@projeto.com",
     receberMarketing: false,
-    ultimaCompra: "12/08/2023",
+    ultimaCompra: "2023-08-15",
   },
   {
-    addressee: "cliente3@projeto.com",
+    addressee: "cliente03@projeto.com",
     receberMarketing: true,
-    ultimaCompra: "12/06/2023",
+    ultimaCompra: "2023-06-02",
   },
   {
-    addressee: "cliente4@projeto.com",
+    addressee: "cliente04@projeto.com",
     receberMarketing: true,
-    ultimaCompra: "02/08/2023",
+    ultimaCompra: "2023-08-10",
   },
 ];
 
--- santanders-coders-logica-de-prog/projeto_envio_emails/main.js
+++ santanders-coders-logica-de-prog/projeto_envio_emails/main.js
@@ -1,12 +1,31 @@
 // main.js
 const clientes = require("./clientes");
 const enviarEmail = require("./envia-email");
+const today = new Date();
+const currentMonth = today.getMonth();
 
-// Percorra a lista de clientes e envie emails de acordo com a preferência
+// Percorra a lista de clientes e envie emails de acordo com as especificacoes
 clientes.forEach((cliente) => {
   if (cliente.receberMarketing) {
-    const subject = "Oferta Especial para Você!";
-    const body = "Confira as nossas últimas ofertas em carros novos.";
-    enviarEmail(cliente.addressee, subject, body);
+    const ultimaCompraDate = new Date(cliente.ultimaCompra);
+    const ultimaCompraMonth = ultimaCompraDate.getMonth(); // Mês da última compra
+
+    // Verifica se a última compra foi no mês anterior ou no mês atual
+    if (
+      currentMonth === ultimaCompraMonth ||
+      currentMonth - 1 === ultimaCompraMonth
+    ) {
+      if (today.getDay() === 1) {
+        const subject = "Oferta Especial para Você!";
+        const body = "Confira as nossas últimas ofertas em carros novos.";
+        enviarEmail(cliente.addressee, subject, body);
+      } else {
+        console.log("Hoje não é segunda-feira. Nenhum email será enviado.");
+      }
+    } else {
+      console.log(
+        "O cliente não fez uma compra no mês atual ou no mês anterior. Nenhum email será enviado."
+      );
+    }
   }
 });
