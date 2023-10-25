a8bf2e9f5a5ad41343466dd457492bcff055272c
JM <jaomarcello7@gmail.com>
2023-10-24 23:25:02 -0300

    1st commit projeto angular

---
+++ santander-coders-angular1/aula1/todo-list/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { FormsModule } from '@angular/forms';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { CurrencyFormatPipe } from './pipes/currency-format.pipe';
+import { TaskDetailComponent } from './components/task-detail/task-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DateGreaterThanTodayDirective } from './directives/date-greater-than-today.directive';
+import { TaskFiltroComponent } from './components/task-filtro/task-filtro.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskFormComponent,
+    TaskListComponent,
+    CurrencyFormatPipe,
+    TaskDetailComponent,
+    DateGreaterThanTodayDirective,
+    TaskFiltroComponent,
+  ],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ santander-coders-angular1/aula1/todo-list/src/app/components/task-filtro/task-filtro.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-task-filtro',
+  templateUrl: './task-filtro.component.html',
+  styleUrls: ['./task-filtro.component.scss'],
+})
+export class TaskFiltroComponent {
+  @Output() onChangeFiltro = new EventEmitter();
+
+  filtro: string = 'all';
+
+  handleSelect() {
+    this.onChangeFiltro.emit(this.filtro);
+  }
+}
